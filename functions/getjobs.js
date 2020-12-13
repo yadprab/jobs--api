cdaf2c446350ce760f0f5858fb4410b585e58f09
yathavan <yadhavanprabhakar744@gmail.com>
2020-12-13 08:38:43 +0530

    format

--- functions/getjobs.js
+++ functions/getjobs.js
@@ -1,35 +1,30 @@
-const axios = require('axios');
+const axios = require("axios");
 
-exports.handler= (event, context, callback)=>{
-     const val = event.body;
-   const API_URL = `https://jobs.github.com/positions.json?description=${val}` ;
+exports.handler = (event, context, callback) => {
+  const val = event.body;
+  const API_URL = `https://jobs.github.com/positions.json?description=${val}`;
 
-    const URL = `${API_URL}`;
+  const URL = `${API_URL}`;
 
+  const send = (body) => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  };
 
-    const send =(body)=>{
-        callback(null,{
-            statusCode:200,
-            body:JSON.stringify(body),
-            headers:{
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Content-Type'
-            }
+  const getJobs = () => {
+    axios
+      .get(URL)
+      .then((res) => send(res.data))
+      .catch((err) => send(err));
+  };
 
-        })
-    }
-
-    const getJobs =()=>{
-       axios.get(URL)
-            .then(res=>send(res.data))
-            .catch(err=>send(err))
-    }
-
-    if (event.httpMethod= 'GET') {
-
-        getJobs();
-        
-    }
-
-    
-}
+  if ((event.httpMethod = "GET")) {
+    getJobs();
+  }
+};
--- js/main.js
+++ js/main.js
@@ -1,149 +1,93 @@
 //job searchFn
-const jobSearchFn = ()=>{
-   //get form 
-    const form = document.querySelector('form');
+const jobSearchFn = () => {
+  //get form
+  const form = document.querySelector("form");
 
-   //get label
-   const label = document.querySelector('#toggle');
-   
+  //get label
+  const label = document.querySelector("#toggle");
 
-   //get input
+  //get input
 
-   const input = document.querySelector('[type=search]');
+  const input = document.querySelector("[type=search]");
 
-   //get loader
-   const loader = document.querySelectorAll('.loader');
+  //get loader
+  const loader = document.querySelectorAll(".loader");
 
-   //get ul
-   const ul = document.querySelector('.jobs--area');
+  //get ul
+  const ul = document.querySelector(".jobs--area");
 
-
-    //get filter area
-  const filterArea = document.querySelector('.filter--area');
+  //get filter area
+  const filterArea = document.querySelector(".filter--area");
 
   //get buttons
 
-  const filterButtons = filterArea.querySelectorAll('button');
+  const filterButtons = filterArea.querySelectorAll("button");
 
   const buttonArr = [...filterButtons];
 
   //get nav button
 
-  const navButton = document.querySelector('#navigation--button');
+  const navButton = document.querySelector("#navigation--button");
 
+  const container = document.querySelector(".container");
 
- const container = document.querySelector('.container');
+  const inputArea = document.querySelector(".input--area");
 
- const inputArea = document.querySelector('.input--area');
-
-
-
-
-
-
-
-const submitFn = (e)=>{
+  const submitFn = (e) => {
     e.preventDefault();
-   
+
     const value = input.value;
-     
-   if (value.trim()=='') {
-       
-    window.alert('enter the job description')
-       
-   } else {
-    loader.forEach(list=>list.classList.remove('hide'));
 
-    setTimeout(()=>{
-        loader.forEach(list=>list.classList.add('hide'));
+    if (value.trim() == "") {
+      window.alert("enter the job description");
+    } else {
+      loader.forEach((list) => list.classList.remove("hide"));
 
-    },2000)
+      setTimeout(() => {
+        loader.forEach((list) => list.classList.add("hide"));
+      }, 2000);
 
-    fetchData(value);
-       
-   }
+      fetchData(value);
+    }
 
     form.reset();
+  };
+  const time = (date) => {
+    const d = new Date(date).toISOString();
 
-    
+    const forMat = moment().format(d);
 
-    
-}
- const time =(date)=>{
+    const dt = moment(forMat).fromNow();
 
-  const d = new Date(date).toISOString();
+    return dt;
+  };
 
-const forMat = moment().format(d);
-                     
-                     
- const dt = moment(forMat).fromNow()
-
-
-
- return dt;                 
-
-
-
-
-}
-
-
-const fetchData = (val)=>{
- 
-  
- 
-   
-
-    fetch('/.netlify/functions/getjobs',{
-        method:'POST',
-        headers:{
-            
-            
-             'Accept': 'application/json',
-            'Content-Type': 'text/plain',
-           
-        },
-        body:val,
-    }).then(res=>res.json()).then(data=>{
-        console.log(data);
-     
-          const dataArr = [...data];
-          console.log(dataArr);
-
-        if (data== null|| data.length == 0) {
-
-           
-          
-            ul.innerHTML = `
-
-            <li>
+  const fetchData = (val) => {
+    fetch("/.netlify/functions/getjobs", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "text/plain",
+      },
+      body: val,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const dataArr = [...data];
+        if (data == null || data.length == 0) {
+          ul.innerHTML = `<li>
             <section class="not--found">
                 <h2>not found try later</h2>
-
-            </section>
-        </li>
-
-            
-            
-            
-            
-            
-            
-            
-            
-            `
-            
+                </section>
+        </li>`;
         } else {
- 
-            
-         
-
-            ul.innerHTML = data.map(jobs=>{
-               
-
-
-                return `
-                <li class='jobs--list list--animation ${jobs.type.slice(0,4)}${jobs.type.slice(5,9)}'>
+          ul.innerHTML = data
+            .map((jobs) => {
+              return `
+                <li class='jobs--list list--animation ${jobs.type.slice(
+                  0,
+                  4
+                )}${jobs.type.slice(5, 9)}'>
                 <section class="jobs">
                     <section class="img--section list--animation-2">
                         <img src="${jobs.company_logo}"
@@ -151,15 +95,23 @@ const fetchData = (val)=>{
                        </section>
                   
                        <section class="info">
-                        <h2><span class="job-title list--animation-3"  >${jobs.title}</span></h2>
+                        <h2><span class="job-title list--animation-3"  >${
+                          jobs.title
+                        }</span></h2>
                   
                   
                         
-                        <span class="posted-time list--animation-3" >${time(jobs.created_at)}</span>
+                        <span class="posted-time list--animation-3" >${time(
+                          jobs.created_at
+                        )}</span>
                         <article>
                            
-                            <h3 class="job-type list--animation-3">${jobs.type}</h3>
-                            <h3 class="job-location list--animation-3" >${jobs.location}</h3>
+                            <h3 class="job-type list--animation-3">${
+                              jobs.type
+                            }</h3>
+                            <h3 class="job-location list--animation-3" >${
+                              jobs.location
+                            }</h3>
                            
 
 
@@ -168,75 +120,37 @@ const fetchData = (val)=>{
                    
 
                 </section>
-            </li>
-            
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                `
+            </li>`;
+            })
+            .join("");
 
+          const jobsList = ul.querySelectorAll(".jobs--list");
 
+          jobsList.forEach((jobs) =>
+            setTimeout(() => {
+              jobs.classList.remove("list--animation");
+              jobs.classList.add("hover--state");
+            }, 1000)
+          );
 
+          jobsList.forEach((li, index) =>
+            li.addEventListener(
+              "click",
+              (e) => {
+                e.stopPropagation();
+                const target = dataArr[index];
 
-
-
-
-
-            }).join('');
-
-
-            const jobsList = ul.querySelectorAll('.jobs--list');
-            
-            jobsList.forEach(jobs=>setTimeout(()=>{
-                jobs.classList.remove('list--animation');
-                jobs.classList.add('hover--state')
-            }, 1000));
-
-
-            jobsList.forEach((li, index)=>li.addEventListener('click', (e)=>{
-                 e.stopPropagation();
-                 const target = dataArr[index];
-                  
-                  descriptionFn(target)
-
-            }, {
+                descriptionFn(target);
+              },
+              {
                 capture: true,
-            }))
-            
-       }
-
-        
-   
-
-
-
-
-
-
-
-
-
-
-
-    })
-
-      .catch(err=>{
-         
-          container.innerHTML = `
+              }
+            )
+          );
+        }
+      })
+      .catch((err) => {
+        container.innerHTML = `
 
            <section class='error'>
            
@@ -251,30 +165,24 @@ const fetchData = (val)=>{
           
           
           `;
-         
-      })
-
-
-}
-const descriptionFn =(obj)=>{
-
-    if (obj.length == 0 || obj=== null) {
-        return;
-        
+      });
+  };
+  const descriptionFn = (obj) => {
+    if (obj.length == 0 || obj === null) {
+      return;
     }
 
     const dataObj = obj;
 
-    console.log(JSON.stringify(dataObj.description.replace(/\n/g, '')));
+    console.log(JSON.stringify(dataObj.description.replace(/\n/g, "")));
 
-     const main = document.querySelector('main');
+    const main = document.querySelector("main");
 
-     main.classList.add('over');
+    main.classList.add("over");
 
-     inputArea.style.display = 'none';
+    inputArea.style.display = "none";
 
-
-     container.innerHTML = `
+    container.innerHTML = `
      <section class="selected--job">
                 <section class="job--intro">
                     <section class="job--img--section">
@@ -290,7 +198,9 @@ const descriptionFn =(obj)=>{
                       
                     </section>
                     
-                        <a href="${dataObj.company_url}" target="_blank" rel="noopener noreferrer"  class= 'site'>company site</a>
+                        <a href="${
+                          dataObj.company_url
+                        }" target="_blank" rel="noopener noreferrer"  class= 'site'>company site</a>
                          
                 </section>
                 
@@ -298,12 +208,16 @@ const descriptionFn =(obj)=>{
                  <section class="job--description">
 
                   <section>
-                      <h3 id="time">${time(dataObj.created_at)}<span></span>.<span>${dataObj.type}</span></h3>
+                      <h3 id="time">${time(
+                        dataObj.created_at
+                      )}<span></span>.<span>${dataObj.type}</span></h3>
                 <h3 class="job--des--name">${dataObj.title}</h3>
                 <h3 id="job--location--detail">${dataObj.location}</h3>
                   </section>
 
-                <a href="${dataObj.url}" target="_blank" rel="noopener noreferrer" class="site">apply now</a>
+                <a href="${
+                  dataObj.url
+                }" target="_blank" rel="noopener noreferrer" class="site">apply now</a>
 
 
 
@@ -312,317 +226,177 @@ const descriptionFn =(obj)=>{
 
 
             <article class="content--desc">
-                   ${dataObj.description.replace(/\n/g, '')}
+                   ${dataObj.description.replace(/\n/g, "")}
            </article>
         </section>
              <article class="how--to--apply">
                  <h3>how to apply</h3>
-                     ${dataObj.how_to_apply.replace(/\n/g, '')}
-
-            </article>
+                     ${dataObj.how_to_apply.replace(/\n/g, "")}
+                      </article>
 
         </section> 
-
-              
-                
-     
-
-        </section>
-    
-
-        
-       <section class="footer--overlay">
+ </section>
+    <section class="footer--overlay">
         <section class="details">
               <h3>${dataObj.title}</h3>
         <span>${dataObj.company}</span>
         </section>
                          
-     <a href="${dataObj.url}" target="_blank" rel="noopener noreferrer">apply</a>
+     <a href="${
+       dataObj.url
+     }" target="_blank" rel="noopener noreferrer">apply</a>
 
 
-    </section>
+    </section>`;
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-     `
+    inter();
+  };
 
+  const inter = () => {
+    const target = document.querySelector(".how--to--apply");
 
+    const foot = document.querySelector(".footer--overlay");
 
+    if (foot === null || target === null) {
+      return;
+    }
+    let observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          foot.style.display = "none";
+        } else {
+          foot.style.display = "grid";
+        }
+      });
+    });
 
-inter();
-  
+    observer.observe(target);
+  };
 
-
-}
-
-const inter = ()=>{
-  const target = document.querySelector('.how--to--apply');
-  
-  
-  const foot = document.querySelector('.footer--overlay');
-  
-  if (foot=== null||target===null) {
-    return;
-    
-  }
-let observer = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-      if (entry.isIntersecting) {
-
-        foot.style.display = "none";
-        
-      }else{
-
-         foot.style.display = "grid";
-        
-      }
-    })
-});
-  
-
-observer.observe(target);
-  
-
-  
-  
-  
-
-    
-    
-  }
-  
-  
-
-const filterFn =(e)=>{
-
+  const filterFn = (e) => {
     e.preventDefault();
 
     const target = e.target;
 
-    buttonArr.forEach(button=> button.classList.remove('button--active'))
-   
-    target.classList.add('button--active');
-  
-   
-    const attr = target.getAttribute('data-value');
+    buttonArr.forEach((button) => button.classList.remove("button--active"));
 
+    target.classList.add("button--active");
 
-    const li = ul.querySelectorAll('.jobs--list');
+    const attr = target.getAttribute("data-value");
 
-
+    const li = ul.querySelectorAll(".jobs--list");
 
     const jobsArr = [...li];
-    
-    
-    if (jobsArr==null|| jobsArr.length ==0){
-        
-        return;
-        
+
+    if (jobsArr == null || jobsArr.length == 0) {
+      return;
     }
 
-    
+    switch (attr) {
+      case "fulltime":
+        jobsArr.filter((jobs) => {
+          jobs.classList.contains("FullTime")
+            ? (jobs.style.display = "grid")
+            : (jobs.style.display = "none");
+        });
 
-
-
-switch (attr) {
-    case 'fulltime':
-      jobsArr.filter(jobs=>{jobs.classList.contains('FullTime')?jobs.style.display = 'grid': jobs.style.display='none'});
-          
         break;
 
-        case 'parttime':
-            jobsArr.filter(jobs=>{jobs.classList.contains('PartTime')?jobs.style.display = 'grid': jobs.style.display='none'});
-            
-            break;
+      case "parttime":
+        jobsArr.filter((jobs) => {
+          jobs.classList.contains("PartTime")
+            ? (jobs.style.display = "grid")
+            : (jobs.style.display = "none");
+        });
 
-    default: 'All'
-      jobsArr.forEach(jobs=>jobs.style.display = 'grid')
         break;
-}
-   
 
+      default:
+        "All";
+        jobsArr.forEach((jobs) => (jobs.style.display = "grid"));
+        break;
+    }
+  };
 
-
-
-
-
-
-
-    
-}
-
- const theme =(e)=>{
-
+  const theme = (e) => {
     e.preventDefault();
 
-   
-     
-    
-         const themeProp ={
-    
-            theme:'',
-            state:'',
-         }
+    const themeProp = {
+      theme: "",
+      state: "",
+    };
 
+    if (document.body.className == "light") {
+      document.body.className = "dark";
 
+      label.className = "toggle--move";
 
-if (document.body.className == 'light') {
+      themeProp.theme = document.body.className;
 
-    document.body.className = 'dark';
+      themeProp.state = label.className;
 
-    label.className = 'toggle--move';
-
-    themeProp.theme =document.body.className;
-
-
-    themeProp.state =label.className;
-
-
-    localStorage.setItem('theme', JSON.stringify(themeProp));
-
-
-
-    
-} else {
-    document.body.className = 'light';
-
-    label.className = '';
-
-    themeProp.theme = document.body.className;
-
-    themeProp.state =label.className;
-
-    localStorage.setItem('theme', JSON.stringify(themeProp));
-    
-}
-      
-     
-         
-     
-     
-  }
-     
-    
-     
-const setTheme =()=>{
-
-
-    if (localStorage.getItem('theme')== null) {
-
-        return;
-        
+      localStorage.setItem("theme", JSON.stringify(themeProp));
     } else {
-        const currentTheme = JSON.parse(localStorage.getItem('theme'));
+      document.body.className = "light";
 
-        const {theme, state} = currentTheme;
+      label.className = "";
 
-        document.body.className = theme;
+      themeProp.theme = document.body.className;
 
-           label.className = state;
+      themeProp.state = label.className;
 
-        
+      localStorage.setItem("theme", JSON.stringify(themeProp));
     }
-}
+  };
 
- 
+  const setTheme = () => {
+    if (localStorage.getItem("theme") == null) {
+      return;
+    } else {
+      const currentTheme = JSON.parse(localStorage.getItem("theme"));
 
+      const { theme, state } = currentTheme;
 
+      document.body.className = theme;
 
+      label.className = state;
+    }
+  };
 
-
-const navFn = (e)=>{
-
-e.preventDefault();
-
-//get mobile nav
-
-const mobileNav = document.querySelector('.mobile--nav');
-
-
-mobileNav.classList.remove('hide')
-
-setTimeout(()=>{
-    mobileNav.classList.add('show');
-},500);
-
-
-const closeButton = mobileNav.querySelector('#close--button');
-
-
-
-const closeFn =(e)=>{
+  const navFn = (e) => {
     e.preventDefault();
 
-    
-mobileNav.classList.add('hide')
+    //get mobile nav
 
+    const mobileNav = document.querySelector(".mobile--nav");
 
-}
+    mobileNav.classList.remove("hide");
 
+    setTimeout(() => {
+      mobileNav.classList.add("show");
+    }, 500);
 
-closeButton.addEventListener('click', closeFn);
+    const closeButton = mobileNav.querySelector("#close--button");
 
-     
+    const closeFn = (e) => {
+      e.preventDefault();
 
+      mobileNav.classList.add("hide");
+    };
 
-}
+    closeButton.addEventListener("click", closeFn);
+  };
 
+  setTheme();
 
+  filterButtons.forEach((tab) => tab.addEventListener("click", filterFn));
 
+  form.addEventListener("submit", submitFn);
 
+  navButton.addEventListener("click", navFn);
 
+  //click event for label
+  label.addEventListener("click", theme);
+};
 
- 
-
-
-
-
-
-setTheme();
-
-
-filterButtons.forEach(tab=>tab.addEventListener('click',filterFn));
-
-form.addEventListener('submit', submitFn);
-
-navButton.addEventListener('click', navFn);
-
- //click event for label
-  label.addEventListener('click', theme);
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-window.addEventListener('DOMContentLoaded', jobSearchFn);
+window.addEventListener("DOMContentLoaded", jobSearchFn);
